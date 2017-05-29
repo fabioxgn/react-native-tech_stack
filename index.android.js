b3c6202f77e5b6d99ddbe4ce6e152976779f9c4f
Fábio Gomes <fabioxgn@gmail.com>
2017-05-29 19:40:36 -0300

    Adiciona animação

--- index.android.js
+++ index.android.js
@@ -1,4 +1,6 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, UIManager } from 'react-native';
 import App from './src/app';
 
+// Necessário para a animação funcionar no Android
+UIManager.setLayoutAnimationEnabledExperimental(true);
 AppRegistry.registerComponent('tech_stack', () => App);
